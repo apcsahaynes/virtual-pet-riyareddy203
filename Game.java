21e1593c1a9e5e1b86b148d9dffe2986b6fc7201
riyareddy203 <riya.reddy@jpschools.org>
2026-03-09 10:07:41 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,32 @@
 public class Game {
+    private String name;
+    public int happinessIncr;
+    public int weightDecr;
+    
+    
+    public Game (String name, int happinessIncr, int weightDecr) {
+        this.name = name;
+        this.happinessIncr = happinessIncr;
+        this.weightDecr = weightDecr;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public int happinessIncr() {
+        return happinessIncr;
+    }
+    public int weightDecr() {
+        return weightDecr;
+    }
+    public boolean isWinner() {
+        Math.random();
+        if (Math.random()>0.5){
+          return true;
+        }
+        else {
+          return false;
+        }
+    }
     
 }
