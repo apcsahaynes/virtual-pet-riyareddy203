095f7059bd1eb503058abefd29ab8751d860c06d
riyareddy203 <riya.reddy@jpschools.org>
2026-03-09 10:08:29 -0500

    Update VirtualPet.java

--- VirtualPet.java
+++ VirtualPet.java
@@ -1,3 +1,70 @@
 public class VirtualPet {
-    
+  private String name;
+  private int energy;
+  private int weight;
+  private int happiness;
+  private int ageMonths;
+  private int ageYears;
+
+  public VirtualPet(String name){
+    this.name = name;
+    this.energy = 0;
+    this.weight = 5;
+    this.happiness = 0;
+    this.ageMonths = 0;
+    this.ageYears = 0;
+  }
+
+  public String getName(){
+    return name;
+  }
+  
+  public int getEnergyLevel(){
+    return energy;
+  }
+
+  public int getHappinessLevel(){
+    return happiness;
+  }
+
+  public void feed(Food f){
+    if (energy < 10){
+      energy+= f.energyIncrease;
+      weight+= f.weightGain;
+    }
+  }
+
+  public boolean play(Game g){
+    if (happiness < 10){
+      happiness+= g.happinessIncr;
+    }
+    if (weight > 5){
+      weight-= g.weightDecr;
+    }
+    return g.isWinner();
+  }
+
+  public void updateStatus(){
+    if (happiness > 0){
+      happiness--;
+    }
+    if (energy > 0){
+      energy--;
+    }
+    ageMonths++;
+    ageConverter();
+  }
+
+  private void ageConverter(){
+    if (ageMonths >= 12){
+      ageYears++;
+      ageMonths = 0;
+    }
+  } 
+
+  public String toString(){
+    return name + "'s information: \nEnergy: " + energy 
+    + "\nweight: " + weight + "\nHappiness: " + happiness
+    + "\nAge: " + ageYears + " years and " + ageMonths + " months.";
+  }
 }
