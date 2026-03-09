147b8ac31bd951a53f2d8a8bef4f4135f2468ba5
riyareddy203 <riya.reddy@jpschools.org>
2026-03-09 10:07:23 -0500

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,3 +1,28 @@
 public class Food {
+    private String name;
+    public int energyIncrease;
+    public int happinessIncrease;
+    public int weightGain;
+    
+    public Food (String name, int energyIncrease, int happinessIncrease, int weightGain) {
+        this.name = name;
+        this.happinessIncrease = happinessIncrease;
+        this.energyIncrease = energyIncrease;
+        this.weightGain = weightGain;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public int getEnergyIncrease() {
+        return energyIncrease;
+    }
+    public int getHappinessIncrease() {
+        return happinessIncrease;
+    }
+    public int getWeightGain() {
+        return weightGain;
+    }
+    
     
 }
